f9a4333d79deb91005fe32c5b94560342f8c007e
Parth <pm474@njit.edu>
2017-07-29 00:28:08 -0400

    Inserted isValidParanthesis() method

--- StackOperations.java
+++ StackOperations.java
@@ -8,5 +8,28 @@ class StackOperations
 		Scanner sc = new Scanner(System.in);
 	}
 
-	
+	public boolean isValidParanthesis(String s)
+    	{
+        	Stack<Character> stack = new Stack<Character>();
+     
+        	for(char c : s.toCharArray())
+            		switch(c)
+            		{
+                		case '[':
+                    			stack.push(']');
+                    			break;
+                		case '(':
+                    			stack.push(')');
+                    			break;
+                		case '{':
+                    			stack.push('}');
+                    			break;
+                		default:
+                    			if(stack.isEmpty() || c!= stack.pop())
+                        			return false;
+                    		break;
+            		}
+        
+        	return stack.isEmpty();
+    	}
 }
